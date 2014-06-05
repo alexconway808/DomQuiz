9f295eb78450902cc02b5a2bfc21cd05b3a6eb8e
Alex Conway <alexconway808@gmail.com>
2014-06-04 18:16:59 -1000

    problem with innerhtml

--- dom_quiz.js
+++ dom_quiz.js
@@ -1,6 +1,28 @@
 (function(){
 
+var newscore = 0;
+var count = 0;
+var question = document.getElementById('question');
+var answer = document.getElementById('answer');
 
+var questionaire = [
+	"What is the first spot on the North Shore?",
+	"What is the second spot on the North Shore?",
+	"What is the third spot on the North Shore?",
+	"What is the fourth spot on the North Shore?",
+	"What is the fifth spot on the North Shore?",
+];
 
+var correctAnswer = [
+	"Haleiwa",
+	"Laniakea",
+	"Waiamea",
+	"Pipeline",
+	"Sunset",
+	];
 
-})();
+  question.innerHTML = "What is the first spot on the North Shore?";
+
+  var submitButton = document.getElementById('submit');
+
+});
